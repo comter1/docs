391eb6cd41dd35def87635f3ee6cf79aabc7a277
comter1 <1069055297@qq.com>
2021-10-06 19:59:48 +0800

    time

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+const headConf = require("./config/headConf");
+const pluginsConf = require("./config/pluginsConf");
+const navConf = require("./config/navConf");
+module.exports= {
+    base:"/docs/",
+    title:"浪荡人儿",
+    description:"简单笔记",
+    head: headConf,
+    plugins: pluginsConf,
+    themeConfig : {
+      lastUpdated: '最后更新',
+      logo: '/assets/img/logo.png',
+      nav: navConf,
+     /* sidebar:[
+      '',
+      'about',
+      '装机方案',
+      {
+        title: '',   // 必要的
+        path: '//',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/',
+        ]
+      },
+  
+  
+    ]*/
+  }
+  };
+
---
+++ docs/.vuepress/config/secret.js
@@ -0,0 +1,5 @@
+module.exports = {
+    clientId: 'b8863c8b486f71d52002',
+    clientSecret: '5e118a005bca24bd208da01a54057a9349dda3bf',
+    ga: 'G-MDS3WEGK1H'
+}
